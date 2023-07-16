b0a74d1838930d63ac553d06935529fb1c3de3cd
John Hilasaca <89051327+Jaalya@users.noreply.github.com>
2023-07-16 00:02:21 -0500

    Change Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import Sidebar from './admin/menu/Sidebar'
+
 import  { Error } from './pages/Error'
 import HomeAdmin from './admin/HomeAdmin'
-import Autos from './admin/Carros/autos'
-import { CardAutos } from './admin/Carros/CardAutos'
-import { CreateCar } from './admin/components/forms/CreateCar'
-import { EditCar } from './admin/components/forms/EditCar'
-import { CardClientes } from './admin/clientes/CardClientes'
-import Clientes from './admin/clientes/Clientes'
-import { InfoClien } from './admin/clientes/InfoClien'
-import { Config } from './admin/components/config/Config'
-import { Reservas } from './admin/components/Reservas.js/Reservas'
-import { InfoReserva } from './admin/components/Reservas.js/InfoReserva'
-import { CardsReservas } from './admin/components/Reservas.js/CardsReservas'
-import { TablePending } from './admin/components/Reservas.js/TablePending'
-import { TableConcret } from './admin/components/Reservas.js/TableConcret'
-import { ClientsPending } from './admin/clientes/ClientsPending'
-import { ClientsNo } from './admin/clientes/ClientsNo'
-import { Pagos } from './admin/components/pagos/pagos'
-import { Historial } from './admin/components/historial/Historial'
+
 import Navbar from './client/components/Navbar'
 import { HomeClient } from './client/components/HomeClient'
 import { Cars } from './client/components/Cars'
@@ -32,7 +16,7 @@ import Registro from './pages/Registro'
 import { PagosC } from './client/components/PagosC';
 import { ReservasC } from './client/components/ReservasC';
 import { ContactForm } from './client/components/ContactForm';
-import { Profile } from './admin/components/Profile/Profile';
+
 import { EditPerfil } from './client/components/EditPerfil';
 import { RecuperacionContrasena } from './pages/RecuperacionContrasena ';
 function App() {
@@ -70,29 +54,9 @@ function App() {
       {/* ================= ADMINISTRADOR ============0z */}
       <Routes>
         <Route element={<ProtectedRoute usuario={correo} />}>
-          <Route path='/' element={<Sidebar usuario={correo} />}>
+          <Route path='/'>
             {isUserLoggedIn && (
-              <>
-                <Route path='/Home' element={<HomeAdmin />} />
-                <Route path='/Autos' element={<Autos />} />
-                <Route path='/Autos/:id' element={<CardAutos />} />
-                <Route path='/Clientes' element={<Clientes />} />
-                <Route path='/crearAuto' element={<CreateCar />} />
-                <Route path='/EditCar/:id' element={<EditCar />} />
-                <Route path='/CardClient' element={<CardClientes />} />
-                <Route path='/InfoClient/:id' element={<InfoClien />} />
-                <Route path='/Configuracion' element={<Config />} />
-                <Route path='/Reservas' element={<Reservas />} />
-                <Route path='/InfoReserva/:id' element={<InfoReserva />} />
-                <Route path='/ReservaCard' element={<CardsReservas />} />
-                <Route path='/ReservasPendientes' element={<TablePending />} />
-                <Route path='/ReservasConcretas' element={<TableConcret />} />
-                <Route path='/clientesPending' element={<ClientsPending />} />
-                <Route path='/clientesNo' element={<ClientsNo />} />
-                <Route path='/Pagos' element={<Pagos />} />
-                <Route path='/Perfil' element={<Profile id={id_user}/>} />
-                <Route path='/Historial/:id' element={<Historial />} />
-              </>
+              <Route path='/Home/*' element={<HomeAdmin id_user={id_user} correo={correo}/>} />
             )}
           </Route>
 
--- src/admin/HomeAdmin.jsx
+++ src/admin/HomeAdmin.jsx
@@ -1,13 +1,54 @@
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import stil from './HomeAdmin.module.css';
 
-import './Home.css';
 
-export default function HomeAdmin() {
+import Autos from './Carros/Autos'
+import { CardAutos } from './Carros/CardAutos'
+import { CreateCar } from './components/forms/CreateCar'
+import { EditCar } from './components/forms/EditCar'
+import { CardClientes } from './clientes/CardClientes'
+import Clientes from './clientes/Clientes'
+import { InfoClien } from './clientes/InfoClien'
+import { Config } from './components/config/Config'
+import { Reservas } from './components/Reservas.js/Reservas'
+import { InfoReserva } from './components/Reservas.js/InfoReserva'
+import { CardsReservas } from './components/Reservas.js/CardsReservas'
+import { TablePending } from './components/Reservas.js/TablePending'
+import { TableConcret } from './components/Reservas.js/TableConcret'
+import { ClientsPending } from './clientes/ClientsPending'
+import { ClientsNo } from './clientes/ClientsNo'
+import { Pagos } from './components/pagos/pagos'
+import { Historial } from './components/historial/Historial'
+import { Profile } from './components/Profile/Profile';
+
+import Sidebar from './Sidebar'
+
+export default function HomeAdmin({ correo, id_user }) {
   return (
-    <div className="page-content">
-    <div className="home-container">
-      <h1>Bienvenido, Administrador</h1>
-      <p>Esta es tu página de inicio.</p>
-    </div>
-  </div>
+    <section className={stil.pageContent}>
+      <Sidebar correo={correo} />
+      <div className=''>
+        <Routes>
+          <Route path='/Autos' element={<Autos />} />
+          <Route path='/Autos/:id' element={<CardAutos />} />
+          <Route path='/Clientes' element={<Clientes />} />
+          <Route path='/crearAuto' element={<CreateCar />} />
+          <Route path='/EditCar/:id' element={<EditCar />} />
+          <Route path='/CardClient' element={<CardClientes />} />
+          <Route path='/InfoClient/:id' element={<InfoClien />} />
+          <Route path='/Configuracion' element={<Config />} />
+          <Route path='/Reservas' element={<Reservas />} />
+          <Route path='/InfoReserva/:id' element={<InfoReserva />} />
+          <Route path='/ReservaCard' element={<CardsReservas />} />
+          <Route path='/ReservasPendientes' element={<TablePending />} />
+          <Route path='/ReservasConcretas' element={<TableConcret />} />
+          <Route path='/clientesPending' element={<ClientsPending />} />
+          <Route path='/clientesNo' element={<ClientsNo />} />
+          <Route path='/Pagos' element={<Pagos />} />
+          <Route path='/Perfil' element={<Profile id={id_user} />} />
+          <Route path='/Historial/:id' element={<Historial />} />
+        </Routes>
+      </div>
+    </section>
   )
 }
